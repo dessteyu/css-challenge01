74a731d69d6301373fa2a9b1b7ec41188ebbb7bb
dessteyu <bayembye10@gmail.com>
2021-03-28 14:10:54 +0000

    css cahallenge 01 finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="frame">
+      <div class="center">
+        <div class="big-vue">
+          <div class="sun">
+            <div class="stain"></div>
+            <div class="stain1"></div>
+            <div class="small-stain"></div>
+            <div class="small-stain1"></div>
+            <div class="small-stain2"></div>
+            <div class="small-stain3"></div>
+            <div class="small-stain4"></div>
+            <div class="small-stain5"></div>
+            <div class="small-stain6"></div>
+          </div>
+          <div class="dune"></div>
+          <div class="dune1"></div>
+          <div class="dune2"></div>
+          <div class="dune3"></div>
+          <div class="dune4"></div>
+          <div class="rain"></div>
+          <div class="rain1"></div>
+          <div class="rain2"></div>
+          <div class="rain3"></div>
+          <div class="rain4"></div>
+          <div class="rain5"></div>
+          <div class="rain6"></div>
+          <div class="rain7"></div>
+          <div class="rain8"></div>
+          <div class="rain9"></div>
+          <div class="medium-rain"></div>
+          <div class="medium-rain1"></div>
+          <div class="medium-rain2"></div>
+          <div class="medium-rain3"></div>
+          <div class="medium-rain4"></div>
+          <div class="medium-rain5"></div>
+          <div class="medium-rain6"></div>
+          <div class="medium-rain7"></div>
+          <div class="medium-rain8"></div>
+          <div class="medium-rain9"></div>
+          <div class="small-rain"></div>
+          <div class="small-rain1"></div>
+          <div class="small-rain2"></div>
+          <div class="small-rain3"></div>
+          <div class="small-rain4"></div>
+          <div class="small-rain5"></div>
+          <div class="small-rain6"></div>
+          <div class="small-rain7"></div>
+          <div class="small-rain8"></div>
+          <div class="small-rain9"></div>
+          <div class="small-rain10"></div>
+          <div class="small-rain11"></div>
+          <div class="small-rain12"></div>
+          <div class="small-rain13"></div>
+          <div class="small-rain14"></div>
+          <div class="small-rain15"></div>
+          <div class="small-rain16"></div>
+          <div class="small-rain17"></div>
+          <div class="small-rain18"></div>
+          <div class="small-rain19"></div>
+          <div class="small-rain20"></div>
+        </div>
+        <div class="legende">
+          <div class="col1">
+            <div class="deg-value">12°</div>
+            <div class="description">
+              <p>Wind: E 7 km/h</p>
+              <p>Humidity: 87%</p>
+            </div>
+          </div>
+          <div class="col2">
+            <p>TUE 21° / 9°</p>
+            <p>WED 23° / 10°</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
